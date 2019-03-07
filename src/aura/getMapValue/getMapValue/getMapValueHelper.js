cb98475fd0c49c44fe2bd265c06eda6bc3f3b20d
Andrii Leshchuk <36226043+leshchukandrej@users.noreply.github.com>
2019-03-07 10:59:13 +0200

    Add files via upload

---
+++ src/aura/getMapValue/getMapValue/getMapValueHelper.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Andrii Leshchuk on 07.03.2019.
+ */
+({
+
+    getMapValue: function (component) {
+        let map = component.get('v.map');
+        let key = component.get('v.key');
+
+        if (map && key) {
+            component.set('v.returnValues', [map[key]]);
+        }
+    }
+
+})
